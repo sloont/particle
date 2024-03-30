7c6a0a17d4e46e512a35b956bd5a4a0ccfeeb6c2
sloont <colinabel93@gmail.com>
2024-03-30 10:24:53 -0400

    link Gui controls to Scene controls

--- src/DemoScene.ts
+++ src/DemoScene.ts
@@ -2,17 +2,19 @@ import ParticleGui from './ParticleGui';
 import ParticleControl from './ParticleControl';
 import cellsheet from '../assets/particles/exampleCellsheet.png';
 
+type ParticleEmitter = Phaser.GameObjects.Particles.ParticleEmitter;
+type ParticleEmitterConfig = Phaser.Types.GameObjects.Particles.ParticleEmitterConfig;
 export default class DemoScene extends Phaser.Scene {
     config: Phaser.Types.GameObjects.Particles.ParticleEmitterConfig;
     control: ParticleControl;
     gui: ParticleGui;
-
+    emitters: ParticleEmitter[] = [];
     constructor() {
         super();
 
         this.control = new ParticleControl();
         this.gui = new ParticleGui(this, this.control);
-        this.config = this.controlToConfig(this.control);
+        this.config = this.initConfig(this.control);
     }
     init() {
     }
@@ -26,15 +28,17 @@ export default class DemoScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.particles(
-            400,
-            300,
-            'demo',
-            this.config,
-        )
+        this.emitters.push(
+            this.add.particles(
+                400,
+                300,
+                'demo',
+                this.config,
+            )
+        );
     }
 
-    controlToConfig(control: ParticleControl): Phaser.Types.GameObjects.Particles.ParticleEmitterConfig {
+    initConfig(control: ParticleControl): ParticleEmitterConfig {
         return {
             frame: 0,
             speed: { min: control.speed.min, max: control.speed.max },
@@ -42,9 +46,68 @@ export default class DemoScene extends Phaser.Scene {
             alpha: { start: 1, end: control.deathOpacityBase },
             duration: control.duration,
             frequency: control.emitFrequency,
-            scaleX: 1 / control.dimensions.width,
-            scaleY: 1 / control.dimensions.height,
+            scaleX: control.dimensions.width / 32,
+            scaleY: control.dimensions.height / 32,
             emitting: true,
         }
+    }
+
+    // hardcode these for 1 emitter for now, but wrap with fetch by index
+    findEmitter(index: number) {
+        return this.emitters[index];
+    }
+
+    lifespan(emitter: ParticleEmitter, val: number) {
+        emitter.setParticleLifespan(val);
+    }
+
+    alphaEnd(emitter: ParticleEmitter, val: number ) {
+        emitter.setParticleAlpha({ start: 1, end: val });
+    }
+
+    dimensions(emitter: ParticleEmitter, val: { width: number, height: number }) {
+        // this takes receives a value between 1 and 32
+        // need to convert here to appropriate scale
+        emitter.setParticleScale(
+            val.width / 32,
+            val.height / 32,
+        );
+    }
+
+    emitZone(emitter: ParticleEmitter, val: { width: number, height: number }) {
+        // need to remove the old emitZone
+        emitter.clearEmitZones();
+        // need to create a rectangle with this data and use it for a RandomZone
+        emitter.addEmitZone(new Phaser.GameObjects.Particles.Zones.RandomZone(
+            new Phaser.Geom.Rectangle(
+                val.width / -2,
+                val.height / -2,
+                val.width,
+                val.height,
+            ) as Phaser.Types.GameObjects.Particles.RandomZoneSource
+        ));
+    }
+
+    emitFrequency(emitter: ParticleEmitter, val: number) {
+        emitter.setFrequency(val);
+    }
+
+    duration(emitter: ParticleEmitter, val: number) {
+        // stop and kill all particles
+        emitter.stop(true);
+        // restart, advancing 0ms and with new duration
+        emitter.start(0, val);
+    }
+
+    angle(emitter: ParticleEmitter, val: { min: number, max: number }) {
+        emitter.setEmitterAngle({ min: val.min, max: val.max });
+    }
+
+    speed(emitter: ParticleEmitter, val: { min: number, max: number }) {
+        emitter.speed = { min: val.min, max: val.max };
+    }
+
+    radial(emitter: ParticleEmitter, val: boolean) {
+        emitter.setRadial(!val);
     }
 }
--- src/ParticleGui.ts
+++ src/ParticleGui.ts
@@ -4,11 +4,13 @@ import DemoScene from "./DemoScene";
 
 export default class ParticleGui {
     gui: GUI;
+    scene: DemoScene;
     control: ParticleControl;
     constructor(scene: DemoScene, control: ParticleControl) {
         this.gui = new GUI({
             title: 'control',
         });
+        this.scene = scene;
         this.control = control;
 
         this.createGuiControls();
@@ -22,7 +24,13 @@ export default class ParticleGui {
             100,
             10000,
             100,
-        );
+        )
+        .onFinishChange((val: number) => {
+            this.scene.lifespan(
+                this.scene.findEmitter(0),
+                val,
+            );
+        });
 
         // deathOpacityBase
         this.gui.add(
@@ -31,50 +39,80 @@ export default class ParticleGui {
             0,
             1,
             0.05,
-        );
+        )
+        .onFinishChange((val: number) => {
+            this.scene.alphaEnd(
+                this.scene.findEmitter(0),
+                val,
+            );
+        });
 
         // dimensions
         const dimensionsFolder = this.gui.addFolder('dimensions');
+        const onDimensionChange = () => {
+            this.scene.dimensions(
+                this.scene.findEmitter(0),
+                this.control.dimensions,
+            );
+        }
         dimensionsFolder.add(
             this.control.dimensions,
             'width',
-            0,
+            1,
             32,
             1,
-        );
+        )
+        .onFinishChange(onDimensionChange);
         dimensionsFolder.add(
             this.control.dimensions,
             'height',
-            0,
+            1,
             32,
             1,
-        );
+        )
+        .onFinishChange(onDimensionChange);
 
         // emitZone
         const emitZoneFolder = this.gui.addFolder('emitZone');
+        const onEmitZoneChange = () => {
+            if (this.control.emitZone.width > 0 && this.control.emitZone.height > 0) {
+                this.scene.emitZone(
+                    this.scene.findEmitter(0),
+                    this.control.emitZone,
+                );
+            }
+        }
         emitZoneFolder.add(
             this.control.emitZone,
             'width',
             0,
             400,
             5,
-        );
+        )
+        .onFinishChange(onEmitZoneChange);
         emitZoneFolder.add(
             this.control.emitZone,
             'height',
             0,
             300,
             5,
-        );
+        )
+        .onFinishChange(onEmitZoneChange);
 
         // emitFrequency
         this.gui.add(
             this.control,
             'emitFrequency',
-            10,
-            1000,
-            10,
-        );
+            2,
+            250,
+            2,
+        )
+        .onFinishChange((val: number) => {
+            this.scene.emitFrequency(
+                this.scene.findEmitter(0),
+                val,
+            );
+        });
 
         // duration
         this.gui.add(
@@ -83,46 +121,74 @@ export default class ParticleGui {
             0,
             10000,
             100,
-        );
+        )
+        .onFinishChange((val: number) => {
+            this.scene.duration(
+                this.scene.findEmitter(0),
+                val,
+            );
+        });
 
         // angle
         const angleFolder = this.gui.addFolder('angle');
+        const onAngleChange = () => {
+            this.scene.angle(
+                this.scene.findEmitter(0),
+                this.control.angle,
+            );
+        }
         angleFolder.add(
             this.control.angle,
             'min',
             0,
             355,
             5,
-        );
+        )
+        .onFinishChange(onAngleChange);
         angleFolder.add(
             this.control.angle,
             'max',
             this.control.angle.min + 5,
             360,
             5,
-        );
+        )
+        .onFinishChange(onAngleChange);
 
         // speed
         const speedFolder = this.gui.addFolder('speed');
+        const onSpeedChange = () => {
+            this.scene.speed(
+                this.scene.findEmitter(0),
+                this.control.speed,
+            );
+        }
         speedFolder.add(
             this.control.speed,
             'min',
             5,
             100,
             5,
-        );
+        )
+        .onFinishChange(onSpeedChange);
         speedFolder.add(
             this.control.speed,
             'max',
             this.control.speed.min + 5,
             200,
             5,
-        );
+        )
+        .onFinishChange(onSpeedChange);
 
         // fixedRotation
         this.gui.add(
             this.control,
             'fixedRotation',
-        );
+        )
+        .onFinishChange((val: boolean) => {
+            this.scene.radial(
+                this.scene.findEmitter(0),
+                val,
+            );
+        });
     }
 }
